f2492f854ba44bdb4759801f8e355422264c743d
Steven G <41505038+EstenGrove@users.noreply.github.com>
2024-05-29 06:11:34 -0700

    Create noteUtils.ts

---
+++ utils/noteUtils.ts
@@ -0,0 +1,79 @@
+export type KeyType = "white" | "black";
+
+export type NoteLabel =
+	| "C"
+	| "D"
+	| "E"
+	| "F"
+	| "G"
+	| "A"
+	| "B"
+	| "C#"
+	| "Db"
+	| "D#"
+	| "Eb"
+	| "F#"
+	| "Gb"
+	| "G#"
+	| "Ab"
+	| "A#"
+	| "Bb";
+
+export interface INote {
+	type: KeyType; // 'white' | 'black' | 'unknown'
+	label: NoteLabel; // 'C' | 'D#' etc
+	octave: number; // 0-9
+	freq: number; // 00.00 OR 00.0
+}
+
+export type BaseOctave = {
+	[key in NoteLabel]?: number;
+};
+
+const BASE_OCTAVE: BaseOctave = {
+	C: 16.35,
+	"C#": 17.32,
+	D: 18.35,
+	"D#": 19.45,
+	E: 20.6,
+	F: 21.83,
+	"F#": 23.12,
+	G: 24.5,
+	"G#": 25.96,
+	A: 27.5,
+	"A#": 29.14,
+	B: 30.87,
+};
+
+const generateOctave = (base: BaseOctave, octave: number) => {
+	const keys = Object.keys(base);
+	const notes: INote[] = [];
+	keys.forEach((key) => {
+		const baseVal = base[key as keyof BaseOctave] as INote["freq"];
+		let octVal = baseVal;
+		octVal *= octave;
+		const type = key.includes("#") || key.includes("b") ? "black" : "white";
+		const newNote: INote = {
+			type: type as INote["type"],
+			label: key as INote["label"],
+			octave: octave as INote["octave"],
+			freq: octVal as INote["freq"],
+		};
+		notes.push(newNote);
+	});
+	return notes;
+};
+
+const generateOctavesFromBase = (base: BaseOctave, octaveCount: number) => {
+	const octList = Array.from(Array(octaveCount).keys()).filter((x) => x !== 0);
+	const allNotes: INote[] = [];
+	octList.forEach((octNum) => {
+		const newOct = generateOctave(base, octNum);
+		allNotes.push(...newOct);
+	});
+	return allNotes;
+};
+
+const transpose = (freq: number, steps: number) => {
+	return freq * Math.pow(2, steps / 12);
+};
