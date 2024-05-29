20d25a25b31152c38594e78b3afa434030869451
Steven G <41505038+EstenGrove@users.noreply.github.com>
2024-05-29 06:01:14 -0700

    Create reverbUtils.ts

---
+++ utils/reverbUtils.ts
@@ -0,0 +1,37 @@
+const getReverbArrayBuffer = async (
+	url: string
+): Promise<ArrayBuffer | unknown> => {
+	try {
+		const request = await fetch(url);
+		const arrayBuffer = await request.arrayBuffer();
+		return arrayBuffer;
+	} catch (error: unknown) {
+		return error;
+	}
+};
+
+// accepts an array buffer & decode's it's audio data
+const convertBufferToAudioBuffer = async (
+	audioCtx: AudioContext,
+	arrayBuffer: ArrayBuffer
+): Promise<AudioBuffer> => {
+	const decoded = await audioCtx.decodeAudioData(arrayBuffer);
+	return decoded;
+};
+
+const getReverbIR = async (
+	audioCtx: AudioContext,
+	url: string
+): Promise<AudioBuffer> => {
+	const arrayBuffer = (await getReverbArrayBuffer(url)) as ArrayBuffer;
+	const audioBuffer = await convertBufferToAudioBuffer(audioCtx, arrayBuffer);
+
+	// return the impulse response as an AudioBuffer
+	return audioBuffer;
+};
+
+export {
+  convertBufferToAudioBuffer,
+	getReverbArrayBuffer,
+	getReverbIR,
+}
